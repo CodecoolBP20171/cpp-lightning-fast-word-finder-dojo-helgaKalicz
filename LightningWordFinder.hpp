a7a200ae53c07fc2fb0b147b00fab81a9fccefb0
helgaKalicz <kalicz.helga@gmail.com>
2017-11-22 10:39:04 +0100

    working :)

--- LightningWordFinder.hpp
+++ LightningWordFinder.hpp
@@ -7,9 +7,50 @@
 class LightningWordFinder
 {
 public:
+
+    bool isValidCharacter(char c)
+    {
+        std::string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i = 0; i < validChars.size(); ++i)
+        {
+            if (c == validChars[i])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
     std::string getLongestWord(const std::string& text)
     {
-        return "Longest Word";
+
+        std::string word = "";
+        std::string buildingWord = "";
+
+        long textLength = text.size();
+        for (int i = 0; i < textLength - word.size(); ++i)
+        {
+            for (int j = 0; j < textLength - (i+1); ++j)
+            {
+                char asd = text[i+j];
+                if (isValidCharacter(text[i+j]))
+                {
+                    buildingWord += text[i+j];
+                }
+                else
+                {
+                    if (word.size() < buildingWord.size())
+                    {
+                        word = buildingWord;
+                    }
+                    i += buildingWord.size();
+                    j = textLength;
+                    buildingWord = "";
+                }
+            }
+        }
+
+        return word;
     }
 };
 #endif //CPP_LIGHTNING_FAST_WORD_FINDER_DOJO_LIGHTNINGWORDFINDER_HPP
